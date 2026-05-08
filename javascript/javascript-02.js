819ea47f43ab43ed3bd19277c09275e4b436b3c6
Erik-Sorcia <pandaeloso10@gmail.com>
2026-05-07 23:36:46 -0600

    Calculadora Con Parametros

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,107 +1,86 @@
-
-let botones = document.getElementsByTagName("button");
+let botones = document.getElementsByClassName("numeros");
 let resultado = document.getElementById("resultado");
-let prm;
-let prm2;
+let prms = [];
 let operacion;
 
 for (const key in botones) {
     if (Object.prototype.hasOwnProperty.call(botones, key)) {
         const boton = botones[key];
-        console.log(boton);
         boton.addEventListener("click", pintar);
     }
 }
 
 function pintar(e) {
-    let valor = e.target.innerText;
-    if (valor == "+") {
-        sumar(e);
-    }
-    else if (valor == "-") {
-        restar(e);
-    }
-    else if (valor == "*"){
-        multiplicar(e);
-    }
-    else if (valor == "/"){
-        dividir(e);
-    }
-    else if (valor == "=") {
-        igual();
-    }
-    else if (valor == "c") {
-        limpiar(e)
-    }
-    else {
-        resultado.value += valor;
-    }
+    resultado.value += e.target.innerText;
 }
 
-let operadores = document.getElementsByClassName("numeros");
-console.log(operadores);
+let operadores = document.getElementsByClassName("operadores");
 
 for (const key in operadores) {
-
     if (Object.prototype.hasOwnProperty.call(operadores, key)) {
         const boton = operadores[key];
-        console.log(boton);
-        boton.addEventListener("click", pintar2);
+        if (boton.innerText == "c" || boton.innerText == "Ce") {
+            boton.addEventListener("click", borrar);
+        } else if (boton.innerText == "=") {
+            boton.addEventListener("click", igual);
+        } else {
+            boton.addEventListener("click", obtener_parametros);
+        }
     }
 }
 
-function pintar2(e) {
-    console.log(e.target.innerText);
-}
-
-function limpiar(e) {
+function borrar() {
     resultado.value = "";
-    prm = "";
-    prm2 = "";
+    prms = [];
     operacion = "";
 }
 
-function sumar(e) {
-    prm = resultado.value;
+function obtener_parametros(e) {
+    prms.push(resultado.value);
     operacion = e.target.innerText;
     resultado.value = "";
-    e.target.style.backgroundColor = "red";
-}
-
-function restar(e) {
-    prm = resultado.value;
-    operacion = e.target.innerText;
-    resultado.value = "";
-    e.target.style.backgroundColor = "red";
-}
-
-function multiplicar(e) {
-    prm = resultado.value;
-    operacion = e.target.innerText;
-    resultado.value = "";
-    e.target.style.backgroundColor = "red";
-}
-
-function dividir(e) {
-    prm = resultado.value;
-    operacion = e.target.innerText;
-    resultado.value = "";
-    e.target.style.backgroundColor = "red";
 }
 
 function igual() {
-    prm2 = resultado.value;
-    if (operacion == "+") {
-        resultado.value = parseInt(prm) + parseInt(prm2);
-    }
-    if (operacion == "-") {
-        resultado.value = parseInt(prm) - parseInt(prm2);
-    }
-    if (operacion == "*") {
-        resultado.value = parseInt(prm) * parseInt(prm2);
-    }
-    if (operacion == "/") {
-        resultado.value = parseInt(prm) / parseInt(prm2);
-    }
-}
+    prms.push(resultado.value);
+    let r = 0;
+    let bandera = false;
+    let op = operacion;
 
+    for (const key in prms) {
+        if (Object.prototype.hasOwnProperty.call(prms, key)) {
+            const prm = prms[key];
+            switch (op) {
+                case "+":
+                    r += parseInt(prm);
+                    break;
+                case "-":
+                    if (bandera == false) {
+                        r = parseInt(prm);
+                        bandera = true;
+                    } else {
+                        r -= parseInt(prm);
+                    }
+                    break;
+                case "*":
+                    if (bandera == false) {
+                        r = parseInt(prm);
+                        bandera = true;
+                    } else {
+                        r *= parseInt(prm);
+                    }
+                    break;
+                case "/":
+                    if (bandera == false) {
+                        r = parseInt(prm);
+                        bandera = true;
+                    } else {
+                        r /= parseInt(prm);
+                    }
+                    break;
+            }
+        }
+    }
+    resultado.value = r;
+    prms = [];
+}
